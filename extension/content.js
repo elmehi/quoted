c1f3d10bb69cd8f6f1e6a32e4e7bee4606a386c0
Eric Mitchell <eam6@princeton.edu>
2016-03-28 00:23:27 -0400

    Changed to applesauce.

--- extension/content.js
+++ extension/content.js
@@ -1,4 +1,1 @@
-document.body.innerHTML = document.body.innerHTML.replace(new RegExp(" the ", "g"), " shit ");
-document.body.innerHTML = document.body.innerHTML.replace(new RegExp(" the ", "g"), " shit ");
-document.body.innerHTML = document.body.innerHTML.replace(new RegExp(" the ", "g"), " shit ");
-document.body.innerHTML = document.body.innerHTML.replace(new RegExp(" the ", "g"), " shit ");
+document.body.innerHTML = document.body.innerHTML.replace(new RegExp(" the ", "g"), " apples ");
