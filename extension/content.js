16e97cc728967e11ef74883dc0e0466db1123b91
Eric Mitchell <eam6@princeton.edu>
2016-03-28 00:17:47 -0400

    Initial code commit.

---
+++ extension/content.js
@@ -0,0 +1,4 @@
+document.body.innerHTML = document.body.innerHTML.replace(new RegExp(" the ", "g"), " shit ");
+document.body.innerHTML = document.body.innerHTML.replace(new RegExp(" the ", "g"), " shit ");
+document.body.innerHTML = document.body.innerHTML.replace(new RegExp(" the ", "g"), " shit ");
+document.body.innerHTML = document.body.innerHTML.replace(new RegExp(" the ", "g"), " shit ");
